ab54c5efef38f853befb5283fe5f2b17dee53352
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-27 10:52:25 +0900

    leetCode 11 BF

---
+++ leetCode/11.ContainerWithMostWater.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+const area = (a, b) => Math.abs(a[0] - b[0]) * Math.min(a[1],b[1])
+
+const maxArea = (heights) => {
+  const points = heights.map( (h, i) => [i, h])
+  let maximum = 0
+  for (let i = 0; i < points.length; i++) {
+    for (let j = 0; j < points.length; j++) {
+      maximum = Math.max(maximum, area(points[i], points[j]))
+    }
+  }
+  return maximum
+};
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]) == 49)
