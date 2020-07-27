2108c37e3ad20913737de5a9aa76bb70e018516a
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-27 11:09:04 +0900

    optimize leetCode 11

--- leetCode/11.ContainerWithMostWater.BF.js
+++ leetCode/11.ContainerWithMostWater.BF.js
@@ -7,19 +7,13 @@ const area = (a, b) => (b[0] - a[0]) * Math.min(a[1],b[1])
 
 const maxArea = (heights) => {
   const points = heights.map( (h, i) => [i, h])
-  let left = 0
-  let right = points.length -1
-
   let maximum = 0
-  while(left < right) {
-    maximum = Math.max(maximum, area(points[left], points[right]))
-    if (points[left][1] > points[right][1])
-      right--
-    else
-      left++
+  for (let i = 0; i < points.length; i++) {
+    for (let j = i+1; j < points.length; j++) {
+      maximum = Math.max(maximum, area(points[i], points[j]))
+    }
   }
   return maximum
 };
 
-console.log(maxArea([1,2,4,3]) == 4)
 console.log(maxArea([1,8,6,2,5,4,8,3,7]) == 49)
