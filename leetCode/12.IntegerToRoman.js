ec2b9c2975fd77fd622b60ab83af5c2920101923
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-27 15:34:43 +0900

    leetCode 12

---
+++ leetCode/12.IntegerToRoman.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+
+const divide = (num, divisor) => {
+  const quotient = parseInt(Math.floor(num / divisor), 10)
+  const remainder = num % divisor
+  return { quotient, remainder}
+}
+
+const toRoman = (divisor, letter) => (num) => {
+  // console.log('toRoman', divisor,letter)
+  const { quotient, remainder } = divide(num, divisor);
+  console.log(quotient, remainder)
+  return {
+    roman: letter.repeat(quotient),
+    remainder
+  }
+}
+
+const toRomans = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
+                .map(([divisor,letter]) => toRoman(divisor, letter))
+
+const intToRoman = (num) => {
+  let resultRoman = ''
+  let currentReminder = num
+  toRomans.forEach(toRoman => {
+    const { roman, remainder } = toRoman(currentReminder)
+    resultRoman += roman
+    currentReminder = remainder
+  })
+  return resultRoman
+};
+
+console.log(intToRoman(17))
