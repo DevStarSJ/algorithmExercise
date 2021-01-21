0f5eff90505b0850f8e68ba8c422cddb0f3e061c
seokjoon.yun <seokjoon.yun@gmail.com>
2021-01-21 11:23:04 +0900

    leetCode 83

--- leetCode/83.RemoveDuplicatesFromSortedList.js
+++ leetCode/83.RemoveDuplicatesFromSortedList.js
@@ -18,33 +18,12 @@ class ListNode1 {
 
 let checkList = new Array(1001).fill(0)
 
-const arrayToLinkedList = (arr) => {
-  let head = new ListNode1(arr[0])
-  let current = head
-  for (let i = 1; i < arr.length; i++) {
-    const newNode = new ListNode1(arr[i])
-    current.next = newNode
-    current = newNode
-  }
-  return head
-}
-
-const linkedListToArray = (head) => {
-  let arr = []
-  let current = head
-  while (current !== undefined) {
-    arr.push(current.val)
-    current = current.next
-  }
-  return arr
-}
-
 var deleteDuplicates = (head) => {
-  const head1 = arrayToLinkedList(head)
-  if (head1.next === undefined) return head1
-  let before = head1
-  let current = head1.next
-  checkList[head1.val] = 1
+  // const head = arrayToLinkedList(head)
+  if (head.next === undefined) return head
+  let before = head
+  let current = head.next
+  checkList[head.val] = 1
 
   while (current !== undefined) {
     if (checkList[current.val] === 0) {
@@ -55,10 +34,56 @@ var deleteDuplicates = (head) => {
     }
     current = current.next
   }
-  return linkedListToArray(head1)
+  return head//linkedListToArray(head)
 };
 
 const arr = [1,1,2,3,3]
 // const arr = [3,4,3,6]
 
 console.log(deleteDuplicates(arr))
+
+// const arrayToLinkedList = (arr) => {
+//   let head = new ListNode1(arr[0])
+//   let current = head
+//   for (let i = 1; i < arr.length; i++) {
+//     const newNode = new ListNode1(arr[i])
+//     current.next = newNode
+//     current = newNode
+//   }
+//   return head
+// }
+
+// const linkedListToArray = (head) => {
+//   let arr = []
+//   let current = head
+//   while (current !== undefined) {
+//     arr.push(current.val)
+//     current = current.next
+//   }
+//   return arr
+// }
+
+// LeetCode에 제출한 코드
+/* 
+var deleteDuplicates = (head) => {
+  if (head == null) return head
+  if (head.next === null) return head
+    
+  let checkList = new Array(2001).fill(0)
+
+  let before = head
+  let current = head.next
+  checkList[head.val+100] = 1
+
+  while (current !== null) {
+    if (checkList[current.val+100] === 0) {
+      checkList[current.val+100] = 1
+      before = current
+    } else {
+      before.next = current.next
+    }
+    current = current.next
+  }
+  return head
+};
+*/
